08bdcc5d52faa44611f0292bfa62d90a9f48178c
wsybw <150867742+ybw0917@users.noreply.github.com>
2023-12-06 16:03:55 -0500

    Create classes

---
+++ Veggie.py
@@ -0,0 +1,24 @@
+from FieldInhabitant import FieldInhabitant
+
+
+class Veggie(FieldInhabitant):
+    def __init__(self, name, symbol, value):
+        FieldInhabitant.__init__(self, symbol)
+        self.__name = name
+        self.__value = value
+
+    def getValue(self):
+        return self.__value
+
+    def getName(self):
+        return self.__name
+
+    def setValue(self, newValue):
+        self.__value = newValue
+
+    def setName(self, newName):
+        self.__name = newName
+
+    def __str__(self):
+        return f"{self.getSymbol()}: {self.getName()}   ,{self.getValue()} points"
+
