84cecf0c341d7a3861152da1fdc49a4c1ae693cb
dishboat <150854370+dishboat@users.noreply.github.com>
2023-12-07 20:44:03 -0500

    Update GameEngine.py
    define init Veggies from file

--- GameEngine.py
+++ GameEngine.py
@@ -1,7 +1,6 @@
 from Veggie import Veggie
 from Captain import Captain
 from Rabbit import Rabbit
-from Snake import Snake
 import pickle
 import os
 import random
@@ -18,3 +17,29 @@ class GameEngine:
         self.__field = []
         self.__rabbit_list = []
         self.__veggie_list = []
+
+    def initVeggies(self):
+        filename = input("Please enter the name of the vegetable point file: ")
+        while not os.path.exists(filename):
+            filename = input(f"{filename} does not exist! Please enter the name of the vegetable point file: ")
+        with open(filename) as point_file:
+            for line in point_file:
+                if "Size" in line:          # Read the first line
+                    temp = line.split(",")
+                    for i in range(int(temp[1])):
+                        col = []
+                        for j in range(int(temp[2])):
+                            col.append(None)
+                        self.__field.append(col)
+                else:
+                    temp = line.split(",")
+                    veggie = Veggie(temp[0], temp[1], int(temp[2]))
+                    self.__veggie_list.append(veggie)
+        for i in range(self.__NUMBEROFVEGGIES):
+            y = random.randrange(0, len(self.__field))
+            x = random.randrange(0, len(self.__field[1]))
+            while self.__field[y][x] is not None:       # make sure the chosen location is not occupied.
+                y = random.randrange(0, len(self.__field))
+                x = random.randrange(0, len(self.__field[1]))
+            type1 = random.randrange(len(self.__veggie_list))
+            self.__field[y][x] = self.__veggie_list[type1].getSymbol()
