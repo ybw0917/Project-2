7c8ba10992f330ba9de084fc49a4f838d9ee0d91
dishboat <150854370+dishboat@users.noreply.github.com>
2023-12-07 20:29:20 -0500

    Update GameEngine.py
    Add basic class.

--- GameEngine.py
+++ GameEngine.py
@@ -1,1 +1,20 @@
+from Veggie import Veggie
+from Captain import Captain
+from Rabbit import Rabbit
+from Snake import Snake
+import pickle
+import os
+import random
 
+
+class GameEngine:
+    def __init__(self):
+        self.__NUMBEROFVEGGIES = 30
+        self.__NUMBEROFRABBITS = 5
+        self.__HIGHSCOREFILE = "highscore.data"
+        self.__score = 0
+        self.__cpt = None
+        self.__snake = None
+        self.__field = []
+        self.__rabbit_list = []
+        self.__veggie_list = []
